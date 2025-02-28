2e31301c5ba4fc98c6f6f55c354ba69017f6e9c7
prakshark <prakhar112358@gmail.com>
2025-02-28 14:38:35 +0530

    final Commit

---
+++ src/components/Banner/StockButton.js
@@ -0,0 +1,11 @@
+const StockButton = () => {
+    return(
+        <>
+            <a href="https://6648b5bdabb34aded4c6e4c0--meek-mooncake-44c0b3.netlify.app/">
+            <button style={{backgroundColor: "black", color: "yellow", marginLeft: "20px", marginTop: "20px", scale: "1", borderRadius: "10px", paddingLeft: "10px", paddingRight: "10px"}}>Latest Stock Prices</button>
+            </a>
+        </>
+    )
+}
+
+export default StockButton;
---
+++ src/components/Header.js
@@ -0,0 +1,81 @@
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import {
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+import BlogButton from "./Banner/BlogButton";
+import StockButton from "./Banner/StockButton";
+import CalculatorButton from "./Banner/CalculatorButton";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#fff",
+    },
+    type: "dark",
+  },
+});
+
+function Header() {
+  const classes = useStyles();
+  const { currency, setCurrency } = CryptoState();
+
+  const history = useHistory();
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+    <h4 style={{marginLeft: "10px", paddingTop: "10px"}}>Other Services: </h4> 
+      <BlogButton/>
+      <StockButton/>
+      <CalculatorButton/>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => history.push(`/`)}
+              variant="h6"
+              className={classes.title}
+            >
+              Team: NodeCodeMode
+            </Typography>
+
+            {/* <Button color="inherit">Login</Button> */}
+            <Select
+              variant="outlined"
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={currency}
+              style={{ width: 100, height: 40, marginLeft: 15 }}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+}
+
+export default Header;
